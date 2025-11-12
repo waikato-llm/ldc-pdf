725f258e30c36be8e8baf19349ec2fb238e774ee
Peter Reutemann <fracpete@gmail.com>
2025-11-13 10:16:27 +1300

    removed "License" classifier (deprecated)

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
     url="https://github.com/waikato-llm/ldc-pdf",
     classifiers=[
         'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: MIT License',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Programming Language :: Python :: 3',
     ],
